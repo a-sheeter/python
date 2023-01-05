0ea5b86ab289e361c2f7eb7667de3a3c45b94a0b
Amy Sheeter <90341586+a-sheeter@users.noreply.github.com>
2023-01-05 14:03:56 -0500

    Create challenges-advanced-strings.py
    Python code challenges: Tests critical thinking skills to write programs that accomplish separate tasks. Each mini test will be separated by comments explaining the challenge

---
+++ challenges-advanced-strings.py
@@ -0,0 +1,82 @@
+# Python code challenges; Tests critical thinking skills to write programs that accomplish separate tasks; Each mini test will be separated by comments explaining the challenge
+
+# Python Challenges: STRINGS
+# 1. Count Letters - Count the number of unique letters in a string. This means that when we are looking at the word, any new letters should be counted and any duplicates should not be counted. There are a few ways to solve this, but we can use the provided alphabet string to ensure that duplicates are not counted. 
+
+def unique_english_letters(string):
+  counter = 0;
+  letters_used = []
+
+  for letter in string:
+    letters_used.append(letter)
+    if letters_used.count(letter) == 1:
+      counter += 1
+  return counter
+
+#test the function
+print(unique_english_letters("mississippi"))
+# should print 4
+print(unique_english_letters("Apple"))
+# should print 4
+
+# 2. Count X - Count the number of occurrences of a certain letter within a string. Our function will accept a parameter for a string and another for the character which we are going to count.
+
+def count_char_x(word, x):
+  counter = 0
+  for letter in word:
+    if letter == x:
+      counter += 1
+  return counter
+
+#test function
+print(count_char_x("mississippi", "s"))
+# should print 4
+print(count_char_x("mississippi", "m"))
+# should print 1
+
+# 3. Count Multi X: This function should accept a string and a substring to compare against. The number of occurrences of the second parameter within the first parameter string are returned. What this means is that we are checking the number of occurrences of the shorter string (second parameter) within the longer string (first parameter). 
+
+def count_multi_char_x(word, x):
+  split_string = word.split(x)
+  return len(split_string) - 1
+
+# test function
+print(count_multi_char_x("mississippi", "iss"))
+# should print 2
+print(count_multi_char_x("apple", "pp"))
+# should print 1
+
+# 4. Substring Between: Create a function that is able to extract a portion of a string between two characters. The function will take three parameters where the first parameter is the string we are going to extract the substring from, the second input is the starting character of our substring and the third character is the ending character of our substring. 
+
+def substring_between_letters(word, start, end):
+  start_index = word.find(start)
+  end_index = word.find(end)
+
+  if start_index != -1 and end_index != -1:
+    return word[start_index+1:end_index]
+  else:
+    return word
+
+#test function
+print(substring_between_letters("apple", "p", "e"))
+# should print "pl"
+print(substring_between_letters("apple", "p", "c"))
+# should print "apple"
+
+# 5. X Length: We need a new function that is able to accept two inputs: one for a sentence and another for a number. The function returns True if every single word in the sentence has a length greater than or equal to the number provided.
+
+def x_length_words(sentence, num):
+  sent_array = sentence.split(" ")
+  for word in sent_array:
+    if len(word) < num:
+      return False
+    else:
+      return True
+
+#test function
+print(x_length_words("i like apples", 2))
+# should print False
+print(x_length_words("he likes apples", 2))
+# should print True
+
+
